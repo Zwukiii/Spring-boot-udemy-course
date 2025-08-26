23db186bf2ccac96545aab18668afcc3ef727c09
Alen <Zwukii@gmail.com>
2025-08-26 19:16:55 +0200

    Done some sections done for today

---
+++ 04-Qualifiers/src/main/java/com/tutorial/springtutorial/common/baseballCoach.java
@@ -0,0 +1,12 @@
+package com.tutorial.springtutorial.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class baseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting  practice";
+    }
+}
