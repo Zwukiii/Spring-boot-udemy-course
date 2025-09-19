24927ef591b217b96d2819e41e98d1ff8b21a635
Alen <Zwukii@gmail.com>
2025-09-19 15:50:29 +0200

    Done with section 4

---
+++ 04-spring-boot-rest-crud/51-openapi-swagger-start-project-rest-api/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
